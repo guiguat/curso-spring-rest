68cf98b9a013d59844b4d5d9e6006d1a284b6c6b
guiguat <guaturadzn@gmail.com>
2020-08-17 14:49:18 -0300

    initialize spring boot

---
+++ curso-spring/src/main/java/com/guilherme/cursospring/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.guilherme.cursospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
