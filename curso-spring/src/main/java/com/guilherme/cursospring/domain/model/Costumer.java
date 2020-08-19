86c4babfbd77f53946e1881f5b8579418b42fbd4
guiguat <guaturadzn@gmail.com>
2020-08-19 15:42:38 -0300

    persist data with JPA

--- curso-spring/src/main/java/com/guilherme/cursospring/domain/model/Costumer.java
+++ curso-spring/src/main/java/com/guilherme/cursospring/domain/model/Costumer.java
@@ -1,9 +1,33 @@
 package com.guilherme.cursospring.domain.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity(name = "costumer")
 public class Costumer {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
+
+  @Column(name="costumer_name")
+  @NotBlank
+  @Size(max = 60)
   private String name;
+
+  @NotBlank
+  @Email
+  @Size(max = 255)
   private String email;
+
+  @NotBlank
+  @Size(max = 20)
   private String phone;
 
   public Long getId() {
@@ -36,5 +60,30 @@ public class Costumer {
 
   public void setId(Long id) {
     this.id = id;
+  }
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((id == null) ? 0 : id.hashCode());
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    Costumer other = (Costumer) obj;
+    if (id == null) {
+      if (other.id != null)
+        return false;
+    } else if (!id.equals(other.id))
+      return false;
+    return true;
   }
 }
