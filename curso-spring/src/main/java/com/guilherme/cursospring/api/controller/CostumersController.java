d544a8fd4529bf8058fd28c839ad896387dd1291
guiguat <guaturadzn@gmail.com>
2020-08-19 15:41:34 -0300

    create costumers controller

--- curso-spring/src/main/java/com/guilherme/cursospring/api/controller/CostumersController.java
+++ curso-spring/src/main/java/com/guilherme/cursospring/api/controller/CostumersController.java
@@ -1,27 +1,73 @@
 package com.guilherme.cursospring.api.controller;
 
-import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
+
+import javax.validation.Valid;
 
 import com.guilherme.cursospring.domain.model.Costumer;
+import com.guilherme.cursospring.domain.repository.CostumerRepository;
+import com.guilherme.cursospring.domain.service.RegisterCostumerService;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/costumers")
 public class CostumersController {
-  @GetMapping("/costumers")
+
+  @Autowired
+  private CostumerRepository costumerRepository;
+
+  @Autowired
+  private RegisterCostumerService registerCostumerService;
+
+  @GetMapping
   public List<Costumer> listar(){
-    var costumer1 = new Costumer();
-    costumer1.setId(1L);
-    costumer1.setEmail("guaturadzn@gmail.com");
-    costumer1.setName("Guilherme Guara");
-    costumer1.setPhone("19 99999-99");
-    var costumer2 = new Costumer();
-    costumer2.setId(2L);
-    costumer2.setEmail("iruri@gmail.com");
-    costumer2.setName("Iruri Correios");
-    costumer2.setPhone("29 99999-99");
-    return Arrays.asList(costumer1, costumer2);
+    return costumerRepository.findAll();
+  }
+
+  @GetMapping("/{costumerId}")
+  public ResponseEntity<Costumer> find(@PathVariable Long costumerId){
+    Optional<Costumer> costumer = costumerRepository.findById(costumerId);
+    if(costumer.isPresent()){
+      return ResponseEntity.ok(costumer.get());
+    }
+    return ResponseEntity.notFound().build();
+  }
+
+  @PostMapping
+  @ResponseStatus(HttpStatus.CREATED)
+  public Costumer create(@Valid @RequestBody Costumer costumer){
+    return registerCostumerService.save(costumer);
+  }
+
+  @PutMapping("/{costumerId}")
+  public ResponseEntity<Costumer> update(@PathVariable Long costumerId, @Valid @RequestBody Costumer costumer){
+    if(!costumerRepository.existsById(costumerId)){
+      return ResponseEntity.notFound().build();
+    }
+    costumer.setId(costumerId);
+    costumer = registerCostumerService.save(costumer);
+    return ResponseEntity.ok(costumer);
+  }
+
+  @DeleteMapping("/{costumerId}")
+  public ResponseEntity<Void> delete(@PathVariable Long costumerId){
+    if(!costumerRepository.existsById(costumerId)){
+      return ResponseEntity.notFound().build();
+    }
+    registerCostumerService.delete(costumerId);
+    return ResponseEntity.noContent().build();
   }
 }
