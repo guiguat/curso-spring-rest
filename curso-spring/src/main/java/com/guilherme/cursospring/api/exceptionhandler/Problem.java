be1e63d24d57d34300801945e446529baaab7850
guiguat <guaturadzn@gmail.com>
2020-08-20 14:36:20 -0300

    use offsetDateTime

--- curso-spring/src/main/java/com/guilherme/cursospring/api/exceptionhandler/Problem.java
+++ curso-spring/src/main/java/com/guilherme/cursospring/api/exceptionhandler/Problem.java
@@ -1,6 +1,6 @@
 package com.guilherme.cursospring.api.exceptionhandler;
 
-import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
 import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
@@ -9,7 +9,7 @@ import com.fasterxml.jackson.annotation.JsonInclude.Include;
 @JsonInclude(Include.NON_NULL)
 public class Problem {
   private Integer status;
-  private LocalDateTime dateTime;
+  private OffsetDateTime dateTime;
   private String title;
   private List<Field> fields;
 
@@ -47,11 +47,11 @@ public class Problem {
     this.status = status;
   }
 
-  public LocalDateTime getDateTime() {
+  public OffsetDateTime getDateTime() {
     return dateTime;
   }
 
-  public void setDateTime(LocalDateTime dateTime) {
+  public void setDateTime(OffsetDateTime dateTime) {
     this.dateTime = dateTime;
   }
 
