ea5caed668dd2759fe149540165dee57225721b9
guiguat <guaturadzn@gmail.com>
2020-08-20 14:36:30 -0300

    create comment input model

---
+++ curso-spring/src/main/java/com/guilherme/cursospring/api/model/CommentInput.java
@@ -0,0 +1,16 @@
+package com.guilherme.cursospring.api.model;
+
+import javax.validation.constraints.NotBlank;
+
+public class CommentInput {
+  @NotBlank
+  private String description;
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+}
